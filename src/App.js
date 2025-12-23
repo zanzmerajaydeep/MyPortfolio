e6ceda8be82cff227467e9987dd20a4dbd647db4
zanzmerajaydeep <zanzmerajaydeep5348@example.com>
2025-12-23 12:48:28 +0530

    add 404 page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import BaseLayout from "./components/BaseLayout";
 import {BrowserRouter} from "react-router-dom";
 
 function App() {
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+if (redirect) {
+  window.history.replaceState(null, "", redirect);
+}
+
    return (
       <div>
          <BrowserRouter>
